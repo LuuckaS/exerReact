58da3559f279a40bed24b3109342a24493d40279
LuuckaS <lucasga@dcc.ufba.br>
2017-05-04 19:15:41 -0300

    botão finalizar

--- src/lista.js
+++ src/lista.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 
+
 class Listar extends Component {
 	constructor(){
 		super();
 		this.state = {
 			lista: "",
+			numero: "",
 		};
 	}
 
@@ -21,18 +23,35 @@ class Listar extends Component {
 		});
 	}
 
+
+	finalizar(){
+
+		if( this.state.lista === ""){
+			alert("Lista vazia!");
+		}
+		else{
+			this.setState({
+				numero:1,
+			});
+		}
+		
+	}
+
+
+
   render() {
     return (
     	<div>
-    	<ul>
-      		<li> {this.state.lista} </li>
-      	</ul>
+    		<ul>
+      			<li> {this.state.lista} </li>
+     	 	</ul>
 
 			<label>Adicione um item</label><br></br>
 			<input type="text" id="inform"></input>
 			<button onClick={this.adicionar.bind(this)}>Adicionar</button>
 			<button onClick={this.remover.bind(this)}>Remover</button>
-			<button >Finalizar</button>
+			<button onClick={this.finalizar.bind(this)}>Finalizar</button>
+			<div><br></br>{this.state.numero}</div>	
 		</div>
     );
   }
